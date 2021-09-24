6178e3d61b9b59f69a6af671fe0ebfadce0d4fae
Sahil Ahuja <sahilahuja@gmail.com>
2021-09-24 14:43:49 +0530

    Adding steps

--- gmetri-docs/docusaurus.config.js
+++ gmetri-docs/docusaurus.config.js
@@ -6,13 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  //This is necessary to be able to host this page at https://www.gmetri.com/docs
+  //This should be without the /docs part acc. to docusaurs docs. https://docusaurus.io/docs/next/docusaurus.config.js#url
+  url: 'https://www.gmetri.com',
+  //https://docusaurus.io/docs/next/docusaurus.config.js#baseurl
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gmetrixr', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-docs', // Usually your repo name.
 
   presets: [
     [
@@ -22,13 +25,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/gmetrixr/docusaurus-docs/edit/main/gmetri-docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          editUrl: 'https://github.com/gmetrixr/docusaurus-docs/edit/main/gmetri-docs/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
